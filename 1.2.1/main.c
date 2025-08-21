f94b9f55098d46ded1a3f5d4f173b3646ab3c249
LeinadHO <149202828+LeinadHO@users.noreply.github.com>
2025-08-20 21:48:29 -0300

    Add files via upload

---
+++ 1.2.1/main.c
@@ -0,0 +1,36 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, char* args[])
+{
+	/* Inicialização */
+	if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		printf("Erro ao inicializar o SDL!");
+		exit(0);
+	}
+
+	/* Criação da janela */
+	SDL_Window* window = SDL_CreateWindow("1.2.1", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 200, SDL_WINDOW_SHOWN);
+	if (window == NULL)
+	{
+		printf("Erro ao criar janela!");
+		exit(0);
+	}
+
+	/* Criação do renderizador */
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	if (renderer == NULL)
+	{
+		printf("Erro ao criar renderizador!");
+		exit(0);
+	}
+
+	/* Execução */
+	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0X00);
+	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0X00);
+	SDL_Rect rectangle = {40,20, 10,10};
+	SDL_RenderFillRect(renderer, &rectangle);
+	SDL_Delay(5000);
+}
