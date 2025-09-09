392e9a6444559de42144cc5c77dbac78460b92e6
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2025-09-09 18:49:46 -0300

    Add new collision function and reorganize folders structure

---
+++ funcoes-externas/Colisoes/colidiu.c
@@ -0,0 +1,30 @@
+#include "colidiu.h"
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int RetanguloColidiu(SDL_Rect* r1, SDL_Rect* r2) {
+
+    // Verifica se os dois retângulos foram passados
+    if (r1 == NULL || r2 == NULL) {
+        printf("Erro! Um ou mais retângulos não foram passados!");
+        return -1;
+    }
+
+    // Define os lados dos retângulos
+    int direito_r1, direito_r2, esquerdo_r1, esquerdo_r2, cima_r1, cima_r2, baixo_r1, baixo_r2;
+
+    // Calcula os lados do retângulo 1
+    direito_r1 = r1->x + r1->w;
+    esquerdo_r1 = r1->x;
+    cima_r1 = r1->y;
+    baixo_r1 = r1->y + r1->h;
+
+    // Calcula os lados do retângulo 2
+    direito_r2 = r2->x + r2->w;
+    esquerdo_r2 = r2->x;
+    cima_r2 = r2->y;
+    baixo_r2 = r2->y + r2->h;
+
+    // Se nenhum dos lados estiver se tocando, retorna 0 
+    return !(baixo_r1 < cima_r2 || cima_r1 > baixo_r2 || direito_r1 < esquerdo_r2 || esquerdo_r1 > direito_r2);
+}
---
+++ funcoes-externas/Colisoes/colidiu.h
@@ -0,0 +1,9 @@
+#ifndef COLIDIU_H
+#define COLIDIU_H
+
+#include <SDL2/SDL.h>
+
+// Função que detecta colisões entre dois retângulos
+int RetanguloColidiu(SDL_Rect* r1, SDL_Rect* r2);
+
+#endif
