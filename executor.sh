b11ad6efd9593f0099d4692cd68ad7a06e4b86b4
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2025-09-11 15:51:50 -0300

    Modify script to compile and execute programs properly

--- executor.sh
+++ executor.sh
@@ -2,6 +2,6 @@ gcc $1/main.c \
     funcoes-externas/AUX_WaitEventTimeoutCount/AUX_WaitEventTimeoutCount.c \
     funcoes-externas/Colisoes/colidiu.c \
     -I funcoes-externas/AUX_WaitEventTimeoutCount \
-    -I funcoes-externas/Colisoes
-    -lSDL2 -lSDL2_gfx -o "$1"
+    -I funcoes-externas/Colisoes \
+    -lSDL2 -lSDL2_gfx -o "$1/$1"
 ./$1/$1
