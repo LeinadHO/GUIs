04d7357a121d6d20f4b48f47b5fa64c92c5a295c
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2025-09-14 17:06:11 -0300

    Add new flag for compilator to properly compile SDL_image library

--- executor.sh
+++ executor.sh
@@ -3,5 +3,5 @@ gcc $1/main.c \
     funcoes-externas/Colisoes/colidiu.c \
     -I funcoes-externas/AUX_WaitEventTimeoutCount \
     -I funcoes-externas/Colisoes \
-    -lSDL2 -lSDL2_gfx -o "$1/$1"
+    -lSDL2 -lSDL2_gfx -lSDL2_image -o "$1/$1"
 ./$1/$1
