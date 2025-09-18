7b616ceca2370f02ffd9ac2a46c0559dbcdabc1b
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2025-09-18 19:56:00 -0300

    Add 'Executador()' function

--- 2.1/main.c
+++ 2.1/main.c
@@ -1,23 +1,24 @@
 /* Bibliotecas */
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include "../funcoes-externas/MultiplosCliques/MultiplosCliques.h"
 
 /* Constantes */
 #define LARGURA 1280
 #define ALTURA 720
 
 /* Funções */
-int inicializador(SDL_Window** win, SDL_Renderer** ren);
-int executador();
-int finalizador(SDL_Window** win, SDL_Renderer** ren);
+int Inicializador(SDL_Window** win, SDL_Renderer** ren);
+int Executador(SDL_Window* win, SDL_Renderer* ren);
+int Finalizador(SDL_Window** win, SDL_Renderer** ren);
 
 /* Entry point do programa */
 int main(int argc, char* argv[]) {
-    // Chamada à função inicializador() e o tratamento dos possíveis retornos associados
+    // Chamada à função Inicializador() e o tratamento dos possíveis retornos associados
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
     int sucesso;
-    sucesso = inicializador(&window, &renderer);
+    sucesso = Inicializador(&window, &renderer);
     switch (sucesso) {
         case 1:
             printf("SDL inicializado corretamente!\n");
@@ -33,10 +34,11 @@ int main(int argc, char* argv[]) {
             return sucesso;
     }
 
-    // Chamada à função 'executador()'
+    // Chamada à função 'Executador()'
+    Executador(window, renderer);
 
-    // Chamada à função 'finalizador()' e o tratamento dos possíveis retornos associados
-    sucesso = finalizador(&window, &renderer);
+    // Chamada à função 'Finalizador()' e o tratamento dos possíveis retornos associados
+    sucesso = Finalizador(&window, &renderer);
     switch (sucesso) {
         case 1:
             printf("SDL finalizado com sucesso!\n");
@@ -52,7 +54,7 @@ int main(int argc, char* argv[]) {
 }
 
 /* Função de inicialização do programa */
-int inicializador(SDL_Window** win, SDL_Renderer** ren) {
+int Inicializador(SDL_Window** win, SDL_Renderer** ren) {
     // Inicialização dos subsistemas do SDL
     int iniciou;
     iniciou = SDL_Init(SDL_INIT_EVERYTHING);
@@ -76,8 +78,60 @@ int inicializador(SDL_Window** win, SDL_Renderer** ren) {
     return 1;
 }
 
+/* Função de execução do programa */
+int Executador(SDL_Window* win, SDL_Renderer* ren) {
+
+    // Declaração das variáveis que serão utilizadas durante a execução
+    SDL_Event evt;
+    int isevt;
+    int continuar_execucao = 1;
+    int n = 0;
+    Uint32 espera = 250;
+    MultiplosCliques mc = {0};
+
+    // Loop de execução do programa
+    while (continuar_execucao) {
+        // Construção e exibição do frame
+        SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0x00);
+        SDL_RenderClear(ren);
+        SDL_RenderPresent(ren);
+        
+        // Captura de eventos
+        isevt = SDL_WaitEventTimeout(&evt, espera);
+        // Evento aconteceu
+        if (isevt) {
+            // Eventos de finalização do loop de captura de eventos
+            if (evt.type == SDL_QUIT) {
+                continuar_execucao = 0;
+            }
+
+            // Eventos de clique do mouse
+            if (evt.type == SDL_MOUSEBUTTONDOWN && evt.button.button == SDL_BUTTON_LEFT) {
+                MultiplosCliques_Contador(&mc);
+            }
+
+            // Eventos de usuário
+            if (evt.type == SDL_USEREVENT) {
+                printf("Você clicou %d vez(es) seguidas!\n", n);
+                continuar_execucao = 0;
+            }
+        }
+        // Não teve evento - Timeout
+        else {
+            n = MultiplosCliques_Quantidade(&mc);
+            MultiplosCliques_Reiniciador(&mc);
+            if (n > 0) {
+                SDL_Event evt_usuario;
+                evt_usuario.type = SDL_USEREVENT;
+                evt_usuario.user.code = n;
+                SDL_PushEvent(&evt_usuario);
+            }
+        }
+    }
+}
+
 /* Função de finalização do programa */
-int finalizador(SDL_Window** win, SDL_Renderer** ren) {
+int Finalizador(SDL_Window** win, SDL_Renderer** ren) {
     // Verifica se os componentes foram passados corretamente
     if (*win == NULL) {
         return -1;
