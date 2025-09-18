089e4a812156ea6c2e99974b8c0a3f5da21ad3a1
IME / UERJ <ime@6019-09.ime.uerj.br>
2025-09-18 09:25:20 -0300

    Initial commit for 2.1 solution

---
+++ 2.1/main.c
@@ -0,0 +1,22 @@
+
+def inicializador() {
+    continue;
+}
+
+def executador() {
+    continue;
+}
+
+def finalizador() {
+    continue;
+}
+
+
+int main(int argc, char* argv[]) {
+
+    // Inicialização
+    // Execução
+    // Finalização
+    return 0;
+
+}
