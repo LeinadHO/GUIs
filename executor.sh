7f6cd90a3ef1879bedce8416bc2c1963788b5e8b
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2025-09-18 19:57:01 -0300

    Modify script to include 'MultiplosCliques' library in compilation

--- executor.sh
+++ executor.sh
@@ -1,7 +1,9 @@
 gcc $1/main.c \
     funcoes-externas/AUX_WaitEventTimeoutCount/AUX_WaitEventTimeoutCount.c \
     funcoes-externas/Colisoes/colidiu.c \
+    funcoes-externas/MultiplosCliques/MultiplosCliques.c \
     -I funcoes-externas/AUX_WaitEventTimeoutCount \
     -I funcoes-externas/Colisoes \
+    -I funcoes-externas/MultiplosCliques \
     -lSDL2 -lSDL2_gfx -lSDL2_image -o "$1/$1"
 ./$1/$1
