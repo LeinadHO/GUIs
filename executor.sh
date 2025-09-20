c83e7a7379c830758741e24a365df9f06c8043b0
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2025-09-20 13:43:03 -0300

    Add 'SDL2_ttf' library in compilation

--- executor.sh
+++ executor.sh
@@ -5,5 +5,5 @@ gcc $1/main.c \
     -I funcoes-externas/AUX_WaitEventTimeoutCount \
     -I funcoes-externas/Colisoes \
     -I funcoes-externas/MultiplosCliques \
-    -lSDL2 -lSDL2_gfx -lSDL2_image -o "$1/$1"
+    -lSDL2 -lSDL2_gfx -lSDL2_image  -lSDL2_ttf -o "$1/$1"
 ./$1/$1
