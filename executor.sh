e3a64d669ea014db8683c8044ce384fd458c9b1e
Daniel Henrique Oliveira <odanielhenrique646@gmail.com>
2025-10-09 15:36:03 -0300

    Update script to not consider 'MultiplosCliques' as an include option

--- executor.sh
+++ executor.sh
@@ -1,9 +1,7 @@
 gcc $1/main.c \
     funcoes-externas/AUX_WaitEventTimeoutCount/AUX_WaitEventTimeoutCount.c \
     funcoes-externas/Colisoes/colidiu.c \
-    funcoes-externas/MultiplosCliques/MultiplosCliques.c \
     -I funcoes-externas/AUX_WaitEventTimeoutCount \
     -I funcoes-externas/Colisoes \
-    -I funcoes-externas/MultiplosCliques \
     -lSDL2 -lSDL2_gfx -lSDL2_image  -lSDL2_ttf -o "$1/$1"
 ./$1/$1
